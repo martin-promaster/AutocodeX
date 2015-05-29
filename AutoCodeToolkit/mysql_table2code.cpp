2a64935f9585ba789bc5b42482099e39364db7b7
unknown <djpmp2010@hotmail.com>
2015-05-29 14:29:04 +0800

    Initialize the project
    Initialize the project and submit the source code.

---
+++ AutoCodeToolkit/mysql_table2code.cpp
@@ -0,0 +1,274 @@
+#include "stdafx.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <tchar.h>
+
+#include "mysql_table2code.h"
+
+
+
+char* make_java_method_name(char* name, const char* method_prefix)
+{
+	int nLen = 3 + strlen(name) + 1;
+	char* pRet = new char[nLen];
+	memset(pRet, 0, nLen);
+
+	strcpy_s(pRet, 4, method_prefix);
+	strcat_s(pRet, nLen, name);
+
+	pRet[3] = pRet[3] - 32;
+
+	return pRet;
+
+}
+
+char* make_java_field_name(char *name)
+{
+	char* pRet = new char[strlen(name)+1];
+	memset(pRet, 0, strlen(name) + 1);
+
+	char *q = pRet;
+
+	char* p = name;
+	while (*p)
+	{
+		if (*p == '_')
+		{
+			p++;
+			*p = *p - 32;
+		}
+		*q++ = *p++;
+	}
+
+	return pRet;
+}
+
+
+void mysql_table2code(HWND hWnd)
+{
+	// Get display device.
+	HDC hdc = GetDC(hWnd);
+
+
+	MYSQL* myConn = mysql_init(NULL);
+
+	if (NULL == myConn)
+	{
+		MessageBox(0, L"", L"", MB_OKCANCEL);
+		return;
+	}
+
+	//database configuartion
+	char dbuser[30] = "redmine";
+	char dbpasswd[30] = "123456";
+	char dbip[30] = "172.16.25.148";
+	unsigned int dbport = 3306;
+	char dbname[50] = "redmine";
+	char tablename[50] = "issues";
+	char *query = NULL;
+
+	if ( NULL == mysql_real_connect(myConn, dbip, dbuser, dbpasswd, 
+		dbname,	dbport, NULL, 0) )
+	{
+		const char *errMessage = NULL;
+		errMessage = mysql_error(myConn);
+		MessageBox(0, L"Connect to database error", L"CAUTION", MB_OKCANCEL);
+		return;
+	}
+
+	if ( NULL != mysql_select_db(myConn, dbname))
+	{
+		const char *errMessage = NULL;
+		errMessage = mysql_error(myConn);
+		MessageBox(0, L"select database error", L"CAUTION", MB_OKCANCEL);
+		return;
+	}
+
+	char tmpSql[640];
+	int ret;
+
+	sprintf_s(tmpSql, "select * from %s", tablename);
+
+	if (mysql_real_query(myConn, tmpSql, strlen(tmpSql)))
+	{
+		printf("Error making query: %s !!!\n", mysql_error(myConn));
+	}
+
+
+	// 将结果保存在 MYSQL_RES 结构体中
+	MYSQL_RES* resultset = mysql_store_result(myConn);
+
+	// Prepare fille handle to store the result set.
+	// +
+	DWORD dwBytesRead, dwBytesWritten, dwPos;
+	HANDLE hfile = CreateFile(L"test.java", GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hfile == INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(hfile);
+		return;
+	}
+
+	// 
+	char szJavaClassBegin[] = "public class test {\n";
+	char szJavaClassEnd[] = "\n}";
+	WriteFile(hfile, szJavaClassBegin, strlen(szJavaClassBegin), &dwBytesWritten, NULL);
+	// -
+
+	int nPosX = 10;
+	int nPosY = 50;
+
+	MYSQL_FIELD *field;
+	while (field = mysql_fetch_field(resultset))
+	{
+		J_POJO_OBJ pojo;
+
+		pojo.field_name = make_java_field_name(field->org_name);;
+		pojo.field_type = plt_get_mysql_field_type_name(field->type);
+		pojo.get_method_name = make_java_method_name(pojo.field_name, "get");
+		pojo.set_method_name = make_java_method_name(pojo.field_name, "set");
+
+		// Generate the POJO class file.
+		char output[1024];
+
+		sprintf_s(output, "public %s %s;", pojo.field_type, pojo.field_name);
+		// Calculate the display position.
+		// +
+		TCHAR *pRet = plt_MultiByteToWideChar(output);
+		int nRet = lstrlen(pRet);
+		TextOut(hdc, nPosX, nPosY, pRet, nRet);
+		SIZE size;
+		GetTextExtentPoint(hdc, pRet, nRet, &size);
+		nPosY += size.cy;
+		// -
+		WriteFile(hfile, output, nRet, &dwBytesWritten, NULL);
+
+
+
+		sprintf_s(output, "public %s %s () {", pojo.field_type, pojo.get_method_name);
+		// Calculate the display position.
+		// +
+		pRet = plt_MultiByteToWideChar(output);
+		nRet = lstrlen(pRet);
+		TextOut(hdc, nPosX, nPosY, pRet, nRet);
+		//SIZE size;
+		GetTextExtentPoint(hdc, pRet, nRet, &size);
+		nPosY += size.cy;
+		// -
+		WriteFile(hfile, output, nRet, &dwBytesWritten, NULL);
+
+
+
+		sprintf_s(output, "    return %s; }", pojo.field_name);
+		// Calculate the display position.
+		// +
+		pRet = plt_MultiByteToWideChar(output);
+		nRet = lstrlen(pRet);
+		TextOut(hdc, nPosX, nPosY, pRet, nRet);
+		//SIZE size;
+		GetTextExtentPoint(hdc, pRet, nRet, &size);
+		nPosY += size.cy;
+		// -
+		WriteFile(hfile, output, nRet, &dwBytesWritten, NULL);
+
+
+
+		sprintf_s(output, "public void %s (%s p0) {", pojo.set_method_name, pojo.field_type);
+		// Calculate the display position.
+		// +
+		pRet = plt_MultiByteToWideChar(output);
+		nRet = lstrlen(pRet);
+		TextOut(hdc, nPosX, nPosY, pRet, nRet);
+		//SIZE size;
+		GetTextExtentPoint(hdc, pRet, nRet, &size);
+		nPosY += size.cy;
+		// -
+		WriteFile(hfile, output, nRet, &dwBytesWritten, NULL);
+
+
+
+		sprintf_s(output, "    %s = p0; }", pojo.field_name);
+		// Calculate the display position.
+		// +
+		pRet = plt_MultiByteToWideChar(output);
+		nRet = lstrlen(pRet);
+		TextOut(hdc, nPosX, nPosY, pRet, nRet);
+		//SIZE size;
+		GetTextExtentPoint(hdc, pRet, nRet, &size);
+		nPosY += size.cy + 20;
+		// -
+		WriteFile(hfile, output, nRet, &dwBytesWritten, NULL);
+
+		WriteFile(hfile, L"\n", 1, &dwBytesWritten, NULL);
+
+
+		printf("");
+	}
+
+	WriteFile(hfile, szJavaClassEnd, strlen(szJavaClassEnd), &dwBytesWritten, NULL);
+
+	CloseHandle(hfile);
+
+	/*
+	MYSQL_ROW row;
+	int rowIndex = 0;
+	int colIndex = 1;
+	while (row = mysql_fetch_row(resultset))
+	{
+		rowIndex = 10;
+		for (int t = 0; t < mysql_num_fields(resultset); t++)
+		{
+			char tmp[1024];
+			sprintf_s(tmp, "%s", row[t]);
+
+			TCHAR *pRet = plt_MultiByteToWideChar(tmp);
+			int nRet = lstrlen(pRet);
+			
+			// Calculate the display position.
+			// +
+			TextOut(hdc, rowIndex, 50 * colIndex, pRet, nRet);
+			SIZE size;
+			GetTextExtentPoint(hdc, pRet, nRet, &size);
+			rowIndex += size.cx + 30;
+			// -
+		}
+		colIndex++;
+	}
+	*/
+
+	mysql_close(myConn);
+}
+
+
+TCHAR* plt_MultiByteToWideChar(LPCCH lpMutiByteStr)
+{
+	int nSize = strlen(lpMutiByteStr);
+
+	int nLen = MultiByteToWideChar(CP_ACP, 0, lpMutiByteStr, -1, NULL,NULL);
+
+	TCHAR *lpWideCharStr = new TCHAR[nLen];
+	memset(lpWideCharStr, 0, nLen*2);
+
+	MultiByteToWideChar(CP_ACP, 0, lpMutiByteStr, nSize, lpWideCharStr, nLen);
+
+	return lpWideCharStr;
+
+}
+
+char* plt_get_mysql_field_type_name(enum_field_types field_type)
+{
+	switch (field_type)
+	{
+	case 0:	return "Decimal";
+	case 1:	return "Tiny";
+	case 2:	return "Short";
+	case 3:	return "Long";
+	case 4:	return "Float";
+	case 5:	return "Double";
+	case 6:	return "NULL";
+	case 7:	return "Timestamp";
+	case 12: return "Date";
+	case 253: return "String";
+	default: return "";
+	}
+}
---
+++ AutoCodeToolkit/test.java
@@ -0,0 +1,27 @@
+public class test {
+public Long id;public Long getId () {    return id; }public void setId (Long p0) {    id = p0; }
+public Long trackerId;public Long getTrackerId () {    return trackerId; }public void setTrackerId (Long p0) {    trackerId = p0; }
+public Long projectId;public Long getProjectId () {    return projectId; }public void setProjectId (Long p0) {    projectId = p0; }
+public String subject;public String getSubject () {    return subject; }public void setSubject (String p0) {    subject = p0; }
+public  description;public  getDescription () {    return description; }public void setDescription ( p0) {    description = p0; }
+public  dueDate;public  getDueDate () {    return dueDate; }public void setDueDate ( p0) {    dueDate = p0; }
+public Long categoryId;public Long getCategoryId () {    return categoryId; }public void setCategoryId (Long p0) {    categoryId = p0; }
+public Long statusId;public Long getStatusId () {    return statusId; }public void setStatusId (Long p0) {    statusId = p0; }
+public Long assignedToId;public Long getAssignedToId () {    return assignedToId; }public void setAssignedToId (Long p0) {    assignedToId = p0; }
+public Long priorityId;public Long getPriorityId () {    return priorityId; }public void setPriorityId (Long p0) {    priorityId = p0; }
+public Long fixedVersionId;public Long getFixedVersionId () {    return fixedVersionId; }public void setFixedVersionId (Long p0) {    fixedVersionId = p0; }
+public Long authorId;public Long getAuthorId () {    return authorId; }public void setAuthorId (Long p0) {    authorId = p0; }
+public Long lockVersion;public Long getLockVersion () {    return lockVersion; }public void setLockVersion (Long p0) {    lockVersion = p0; }
+public Date createdOn;public Date getCreatedOn () {    return createdOn; }public void setCreatedOn (Date p0) {    createdOn = p0; }
+public Date updatedOn;public Date getUpdatedOn () {    return updatedOn; }public void setUpdatedOn (Date p0) {    updatedOn = p0; }
+public  startDate;public  getStartDate () {    return startDate; }public void setStartDate ( p0) {    startDate = p0; }
+public Long doneRatio;public Long getDoneRatio () {    return doneRatio; }public void setDoneRatio (Long p0) {    doneRatio = p0; }
+public Float estimatedHours;public Float getEstimatedHours () {    return estimatedHours; }public void setEstimatedHours (Float p0) {    estimatedHours = p0; }
+public Long parentId;public Long getParentId () {    return parentId; }public void setParentId (Long p0) {    parentId = p0; }
+public Long rootId;public Long getRootId () {    return rootId; }public void setRootId (Long p0) {    rootId = p0; }
+public Long lft;public Long getLft () {    return lft; }public void setLft (Long p0) {    lft = p0; }
+public Long rgt;public Long getRgt () {    return rgt; }public void setRgt (Long p0) {    rgt = p0; }
+public Tiny isPrivate;public Tiny getIsPrivate () {    return isPrivate; }public void setIsPrivate (Tiny p0) {    isPrivate = p0; }
+public Date closedOn;public Date getClosedOn () {    return closedOn; }public void setClosedOn (Date p0) {    closedOn = p0; }
+
+}
